fcbd4939ff5c410e4e58c387d9fd0589777d2526
Matheus Olivera <71763292+dukke1234@users.noreply.github.com>
2021-10-27 08:22:55 -0300

    Add files via upload

---
+++ migrations/20211027235468-create-pedido.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Pedidos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      dataPedido: {
+        type: Sequelize.DATEONLY
+      },
+      ClienteId: {
+        allowNull: false,
+        type: sequelize.INTEGER,
+        references: {
+          model: 'clientes',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Pedidos');
+  }
+};
---
+++ models/itemcompra.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ItemCompra extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {      
+      ItemCompra.belongsTo(models.Compra, {
+        foreignKey: 'CompraId', as: 'compra'});
+
+      ItemCompra.belongsTo(models.Produto, {
+        foreignKey: 'ProdutoId', as: 'produto'});
+    }
+  };
+  ItemCompra.init({
+    quantidade: DataTypes.INTEGER,
+    valor: DataTypes.FLOAT,
+    CompraId: DataTypes.INTEGER,
+    ProdutoId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ItemCompra',
+  });
+  return ItemCompra;
+};
